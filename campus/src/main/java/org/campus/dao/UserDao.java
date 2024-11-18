be964f6e79538e2a5e702f42fd91b4d266a8b00b
lujiakang473 <125789353+lujiakang473@users.noreply.github.com>
2024-11-18 13:53:05 +0800

    Add files via upload

--- campus/src/main/java/org/campus/dao/UserDao.java
+++ campus/src/main/java/org/campus/dao/UserDao.java
@@ -1,39 +1,29 @@
 package org.campus.dao;
 
-import java.util.Map;
-
-import javax.annotation.Resource;
-
-import org.campus.controller.UserController;
 import org.campus.pojo.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
-
-
-@Repository
+@Repository("userDao")
 public class UserDao implements IUserDao{
-	private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 	@Autowired
     MongoTemplate mongoTemplate;
+
+	//用户登录
+	@Override
 	public User login(String telephone){
 		Query query = new Query(Criteria.where("telephone").is(telephone));
-		long current=System.currentTimeMillis();
-		
-		Criteria.where("start_time").lte(current).and("end_time").gte(current);
-		
-	   query.fields().exclude("name");
-		
+		//查询
 		User user=mongoTemplate.findOne(query, User.class);
-		
-	    
-		
-		log.info(user.toString());
+		System.out.println(user);
+//		long current=System.currentTimeMillis();
+
+//		Criteria.where("start_time").lte(current).and("end_time").gte(current);
+
+//	    query.fields().exclude("name");
 		return user;
 	}
 }
